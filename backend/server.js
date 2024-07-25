1dbb546f0db9e045fe7cbafe65c647ae37bee7a0
yuvinha <ha.yuvin@gmail.com>
2024-07-25 22:12:23 +0300

    Connecting MongoDB with Express

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+connectDB(); // Connect to MongoDB
+
+const port = process.env.PORT;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
